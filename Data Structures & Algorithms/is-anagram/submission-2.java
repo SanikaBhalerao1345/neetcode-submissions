865c481a58092cdfae63f7f58fdfaaed2947acb2
Sanika Bhalerao <sanikabhalerao6@gmail.com>
2026-09-26 18:28:44 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+      HashMap<Character, Integer> smap = new HashMap<>();
+      HashMap<Character, Integer> tmap = new HashMap<>();
+      for(int i=0; i<s.length(); i++){
+        smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
+      }
+      for(int j=0; j<t.length(); j++){
+        tmap.put(t.charAt(j), tmap.getOrDefault(t.charAt(j),0)+1);
+      }
+
+      return smap.equals(tmap)? true: false;
+    }
+}
